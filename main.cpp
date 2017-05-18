eb92bdbfcbb2ec2708da28935d15de0ecd6cd7ae
mstepany <m.stepa24@gmail.com>
2017-05-19 01:06:33 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,22 +10,25 @@
 using namespace std;
 
 double fun (double y);
-double Euler_1 (double t0, double q0, double tk, double s);
-void Euler_all (int s, double tk, double s, double t0, double q0);
+double Euler_1 (double t0, double q0, double dt);
+void Euler_all (double s, double tk, double t0, double q0);
 
-void fun (double y) {
+double fun (double y) {
         double f;
         f= (E/R)-(y/(C*R));
         return f;        
 };
-void Euler_1 (double q0, double t0, double dt) {
+double Euler_1 (double t0, double q0, double dt) {
         double q;
         q=q0+dt*fun(q0);
         return q;
         cout<<"t: "<<t0+dt<<" q: "<<q<<endl;
 };
-void Euler_all (int s, double tk, double s, double t0, double q0) {
-        
+void Euler_all (double s, double tk, double t0, double q0) {
+        while (t0<tk) {
+                q0=Euler_1 (q0, t0, ((tk-t0)/s));
+                t0=t0+dt;
+        };
 };
 
 int main () {
@@ -35,13 +38,17 @@ int main () {
         cout<<"Enter t0"<<endl;
         double t0;
         std::cin >> t0;
-        cout<<"Enter dt"<<endl;
-        double dt;
-        std::cin >> dt;
+        cout<<"Enter tk"<<endl;
+        double tk;
+        std::cin >> tk;
         cout<<"Enter s"<<endl;
         double s;
         std::cin >> s;
         cout<<"Enter q0"<<endl;
         double q0;
         std::cin >> q0;
+        double dt;
+        dt=(tk-t0)/s;
+        Euler_all (s, tk, t0, q0);
+        return 0;
 }
